7c01ae01f61a1d84a1d7125003123212a0855729
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-02-16 00:04:52 +0700

    question number 3 with python console

--- prac_03/random.py
+++ prac_03/random.py
@@ -7,3 +7,7 @@
 # Answer : The Smallest would be 3 and the largest would be 9
 # Could line 2 have produced a 4?
 # Answer : No, because the number starts with number 3 with a step of 2
+
+# 3) What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# Answer : The smallest number i've seen is 2.530653600129431 and the largest is 5.413129916887714
