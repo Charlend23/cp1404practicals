b046c1b06e1b48795505dbfef70cf5d52ea0999a
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-02-16 00:03:10 +0700

    question number 2 with python console

--- prac_03/random.py
+++ prac_03/random.py
@@ -1,3 +1,9 @@
 # 1) What did you see on line 1?
 # What was the smallest number you could have seen, what was the largest?
 # Answer: The smallest would be 5 and the largest would be 20
+
+# 2) What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Answer : The Smallest would be 3 and the largest would be 9
+# Could line 2 have produced a 4?
+# Answer : No, because the number starts with number 3 with a step of 2
