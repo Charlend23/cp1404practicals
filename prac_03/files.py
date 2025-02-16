afc121cb24aff71d03dfc991a87b59cd30ca3412
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-02-16 19:44:36 +0700

    question number 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,9 +1,13 @@
 # 1) Write code that asks the user for their name, then opens a file called name.txt and writes that name to it. Use open and close for this question.
 
-user_name = input("Enter Your Name: ")
-out_file = open("name.txt", "w")
-print(user_name, file=out_file)
-out_file.close()
+# user_name = input("Enter Your Name: ")
+# out_file = open("name.txt", "w")
+# print(user_name, file=out_file)
+# out_file.close()
 
 # 2) In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name (as above) then prints (note the exact output), Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable! Use open and close for this question.
 
+out_file = open("name.txt", "r")
+user = out_file.readline()
+print(f"Hi {user}!")
+out_file.close()
