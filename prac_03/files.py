bbfea0f4ded8d895ec762fb67f5f96f3fb45495e
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-02-16 19:49:43 +0700

    number 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,7 +7,24 @@
 
 # 2) In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name (as above) then prints (note the exact output), Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable! Use open and close for this question.
 
-out_file = open("name.txt", "r")
-user = out_file.readline()
-print(f"Hi {user}!")
-out_file.close()
+# out_file = open("name.txt", "r")
+# user = out_file.readline()
+# print(f"Hi {user}!")
+# out_file.close()
+
+# 3)
+
+first_number = 0
+second_number = 0
+
+with open("numbers.txt", "r") as out_file:
+    for i in range(2):
+        lines = out_file.readline()
+        if i == 0:
+            first_number = int(lines.strip())
+        elif i == 1:
+            second_number = int(lines.strip())
+total_number = first_number + second_number
+print(total_number)
+
+# 4)
