acd3c77bd46d50a888b26f904871dc2ae0f1b636
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-02-23 18:01:57 +0700

    added "display_subject_details()" function so it would print each subject code, who is the professor and how many student they have

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,10 +9,12 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
+    display_subject_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -21,9 +23,16 @@ def load_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        data.append(parts)
         print(parts)  # See if that worked
         print("----------")
     input_file.close()
+    return data
+
+def display_subject_details(data):
+    for details in data:
+        subject_code, professor_name, total_students = details
+        print(f"{subject_code} is taught by {professor_name} and has {total_students}")
 
 
 main()
