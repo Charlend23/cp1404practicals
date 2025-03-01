a6d540b0d99561f10f3e56ae8bdad2418e84c8d0
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-02 03:44:44 +0700

    added a new function to convert all the names in the list into a title case and made a new line of code to make sure that the loops continues and add them to the dictionary if the names are correct

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,12 +6,20 @@ Actual =
 
 def main():
     user_email = input("Email: ")
-    split_names(user_email)
-    key_to_value = {}
+    email_to_name = {}
+    while user_email != "":
+        user_name = split_names(user_email)
+        full_name = " ".join(convert_to_title(user_name))
+        name_confirmation = input(f"Is Your Name {full_name}? (Y/n) ").lower()
+        if name_confirmation != "" or "y":
+            name_confirmation = input("Name: ")
+        email_to_name[user_email] = name_confirmation
 
 def split_names(email):
     email_list = email.split("@")[0]
     full_name = email_list.split(".")
     return full_name
 
+def convert_to_title(names):
+    return [name.title() for name in names]
 main()
