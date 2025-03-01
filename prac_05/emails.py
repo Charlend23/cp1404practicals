5c808492eb87c83c22ebf519cb3dfd8cec3222a4
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-02 03:51:35 +0700

    amended some line of code because its not working and added a for loop to print out the name and the email and made the code easier to look at and finished the email.py task

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,13 +7,19 @@ Actual =
 def main():
     user_email = input("Email: ")
     email_to_name = {}
+
     while user_email != "":
         user_name = split_names(user_email)
         full_name = " ".join(convert_to_title(user_name))
         name_confirmation = input(f"Is Your Name {full_name}? (Y/n) ").lower()
-        if name_confirmation != "" or "y":
-            name_confirmation = input("Name: ")
-        email_to_name[user_email] = name_confirmation
+
+        if name_confirmation not in ("", "y"):
+            full_name = input("Name: ")
+        email_to_name[user_email] = full_name
+        user_email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 def split_names(email):
     email_list = email.split("@")[0]
@@ -22,4 +28,5 @@ def split_names(email):
 
 def convert_to_title(names):
     return [name.title() for name in names]
+
 main()
