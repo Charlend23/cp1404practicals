8452feb4529a13a15643fa3400c044a5c1d4e5ea
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-01 21:04:10 +0700

    added ".keys()" to make sure that the user input is being search in the dictionary the key part only and completed all of the prac 5 named state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -11,7 +11,7 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    if state_code in CODE_TO_NAME.keys():
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
