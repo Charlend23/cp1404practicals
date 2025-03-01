8f862679c0361de4fa93253fa26cf1bde98d32cb
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-02 02:53:29 +0700

    made a for loop to check each of the element in the "texts" variable and to add 1 counter if there is an existing word.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,5 +8,19 @@ def main():
     user_text = input("Input Text: ")
     texts = user_text.split()
     word_counter = {}
+    for counter in texts:
+        if counter in word_counter:
+            word_counter[counter] += 1
+        else:
+            word_counter[counter] = 1
+
+    # for counter in texts:
+    #     counter.lower()
+    #     if counter in word_counter:
+    #         word_counter[counter] += 1
+    #     else:
+    #         word_counter[counter] = 1
+    # for word, count in word_counter.items():
+    #     print(f"{word} : {count}")
 
 main()
