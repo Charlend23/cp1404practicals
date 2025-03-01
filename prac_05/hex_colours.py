0cf177efad354a1eceb303755e8b3d6465201931
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-01 21:15:41 +0700

    added a constant dictionary with 10 colours and code in the top of the code page

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+COLOUR_AND_CODE = {"Amaranth": "#e52b50", "AliceBlue": "#f0f8ff", "Amber": "#ffbf00", "Beige": "#f5f5dc",
+                   "Black": "#000000", "BlueViolet": "#8a2be2", "Canary": "#ffff99", "Champagne": "#f7e7ce",
+                   "White": "#ffffff", "Wisteria": "#c9a0dc"}
