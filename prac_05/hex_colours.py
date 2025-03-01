a27baa7daf90b5d479a33e9e9f90e417597c0a90
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-01 23:46:20 +0700

    amended the keys in the dictionary and added a function to check and make the input in a lowercase and to finish the task

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,18 @@
-COLOUR_AND_CODE = {"Amaranth": "#e52b50", "AliceBlue": "#f0f8ff", "Amber": "#ffbf00", "Beige": "#f5f5dc",
-                   "Black": "#000000", "BlueViolet": "#8a2be2", "Canary": "#ffff99", "Champagne": "#f7e7ce",
-                   "White": "#ffffff", "Wisteria": "#c9a0dc"}
+COLOUR_AND_CODE = {"amaranth": "#e52b50", "aliceblue": "#f0f8ff", "amber": "#ffbf00", "beige": "#f5f5dc",
+                   "black": "#000000", "blueviolet": "#8a2be2", "canary": "#ffff99", "champagne": "#f7e7ce",
+                   "white": "#ffffff", "wisteria": "#c9a0dc"}
+
+def main():
+    user_colour = get_input("Enter colour to get code: ")
+    while user_colour != "":
+        try:
+            print(COLOUR_AND_CODE[user_colour])
+        except KeyError:
+            print("Invalid Colour")
+        user_colour = get_input("Enter colour to get code: ")
+
+def get_input(prompt):
+        user_input = input(prompt).lower()
+        return user_input
+
+main()
