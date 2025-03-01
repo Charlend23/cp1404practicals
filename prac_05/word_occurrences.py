c7911f0ac6f2fb01938cf16a7e932285ac5d7c9a
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-02 03:06:55 +0700

    made the "texts" variable to be sorted so it matched the final output. and finished the task for the file word_occurrences.py Amend = added a code to check the longest length and inputting it into the print code so that it aligns into a single column

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,13 +8,16 @@ def main():
     user_text = input("Input Text: ")
     texts = user_text.split()
     texts.sort()
+    longest_length = 0
     word_counter = {}
     for counter in texts:
         if counter in word_counter:
             word_counter[counter] += 1
         else:
             word_counter[counter] = 1
+        if len(counter) > longest_length:
+            longest_length = len(counter)
     for word, count in word_counter.items():
-        print(f"{word} : {count}")
+        print(f"{word:{longest_length}} : {count}")
 
 main()
