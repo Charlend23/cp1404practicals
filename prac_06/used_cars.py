7060820f2d4d25b92c896efe4440e537ffd0d048
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-12 12:39:19 +0700

    added my own car object and made sure i can see and viewed the named car, also changes some part of line to make them print out their car name not just "Car"

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -11,11 +11,19 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car("G Wagon", 180)
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    print(f"{my_car.name} has fuel: {my_car.fuel}")
     print(my_car)
+
     limo_car = Car("Limo", 100)
     limo_car.add_fuel(20)
-    print(f"Limo has fuel: {limo_car.fuel}")
+    print(f"{limo_car.name} has fuel: {limo_car.fuel}")
     limo_car.drive(115)
+    print(limo_car)
+
+    charlend_car = Car("Supra", 130)
+    charlend_car.add_fuel(50)
+    print(f"{charlend_car.name} has fuel: {charlend_car.fuel}")
+    charlend_car.drive(175)
+    print(charlend_car)
 
 main()
