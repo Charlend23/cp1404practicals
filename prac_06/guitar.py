344563d9973e4c5a275f5ebf56a66af7d18b4b8a
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-12 13:31:34 +0700

    added the guitar class and __init__ function

---
+++ prac_06/guitar.py
@@ -0,0 +1,10 @@
+"""
+Guitar
+Estimate = 50 Minutes
+"""
+
+class Guitar:
+    def __init__(self, name = "", year = 0, cost = 0):
+        self.name = name
+        self.year = year
+        self.cost = cost
