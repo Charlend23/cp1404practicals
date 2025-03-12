4599dba5ff1706b80290229f6bc6e3d625c6915d
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-12 12:44:13 +0700

    added the class for the programming_language.py and added the __init__ methods

---
+++ prac_06/programming_language.py
@@ -0,0 +1,6 @@
+class ProgrammingLanguage:
+    def __init__(self, language, typing, reflection, year):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
