8379885af4b58338824f1cc59940bcf6f7c834e7
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-23 19:12:43 +0700

    added a loop that asks the user to input a new guitar

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,4 +1,5 @@
 from guitar import Guitar
+from prac_04.list_exercise import user_input
 
 FILENAME = "guitars.csv"
 
@@ -11,5 +12,10 @@ def main():
     guitars.sort()
     for guitar in guitars:
         print(f"{guitar.name} is made in {guitar.year} and it cost ${guitar.cost}")
-
+    guitar_name = input("Enter Guitar Name: ")
+    while guitar_name != "":
+        guitar_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: $"))
+        guitars.append(Guitar(guitar_name, guitar_year, guitar_cost))
+        guitar_name = input("Enter Guitar Name: ")
 main()
