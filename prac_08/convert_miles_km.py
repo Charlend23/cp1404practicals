2fc732d698bb8893fece2df1cd0e5e829319dce9
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-03-30 19:08:26 +0700

    added a new function that will calculate the result and send it back to the output label

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,10 +1,17 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+MILES_TO_KM = 1.60934
+
 class MilesToKmApp(App):
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
+
+    def handle_calculation(self):
+        number = float(self.root.ids.input_miles.text)
+        result = number * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
 
 MilesToKmApp().run()
