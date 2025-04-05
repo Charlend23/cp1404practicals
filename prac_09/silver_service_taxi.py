f5495756434614f03827d3612a6fb0cbfa980d90
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-04-06 00:26:33 +0700

    added a string function that will print out the result wanted, and added a get_fare function that will use the existing code from taxi.py and add the price of the flagfall

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,3 +6,11 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+
+    def __str__(self):
+        return (f"{self.name}, fuel={self.fuel}, odo={self._odometer}, "
+                f"{self.current_fare_distance}km on current fare, "
+                f"{self.price_per_km}/km plus flagfall of {self.flagfall}")
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
