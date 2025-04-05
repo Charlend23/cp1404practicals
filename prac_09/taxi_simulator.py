af6902f6518a2b8fdedc40ce87d190cb314f9f62
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-04-06 06:29:48 +0700

    finished the choose option for the main function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,6 +3,7 @@ from silver_service_taxi import SilverServiceTaxi
 
 def main():
     total_bill = 0
+    current_taxi = None
     taxi_list = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Lets Drive!")
     user_input = input("q)uit, c)hoose, d)rive\n>>> ").lower()
@@ -13,10 +14,16 @@ def main():
             for taxi in taxi_list:
                 print(f"{counter} - {taxi}")
                 counter += 1
+            taxi_choice = int(input("Choose taxi: "))
+            if taxi_choice > len(taxi_list):
+                print("Invalid taxi choice")
+            else:
+                current_taxi = taxi_list[taxi_choice]
+            print(f"Bill to date: ${total_bill:.2f}")
         elif user_input == "d":
             print("d")
         else:
             "Invalid option"
-
+    print()
 
 main()
