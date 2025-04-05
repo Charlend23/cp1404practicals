d0de5996c71aa12bab9e2db51bb4d95879ba8b21
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-04-05 16:06:55 +0700

    added the drive function that will generate how far the number of distance will go only if the reliability of the car is bigger than the random number

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,7 +1,21 @@
 from car import Car
+import random
+
+MIN_NUMBER = 0
+MAX_NUMBER = 100
 
 class UnreliableCar(Car):
 
-    def __init__(self, name, fuel, reliability):
+    def __init__(self, name, fuel, reliability: float):
         super().__init__(name,fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        random_number = random.randint(1, 100)
+        if random_number < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+        return distance_driven
+
+
