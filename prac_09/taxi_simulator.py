6588d86e88a17c1dcd82ee74011ec92729cb9863
Charlend Shi <charlend.shi@my.jcu.edu.au>
2025-04-06 07:02:52 +0700

    finished the drive option and fixed some line of code that doesnt fit right into place or causing errors, and adding some end code for the quit option

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -15,15 +15,31 @@ def main():
                 print(f"{counter} - {taxi}")
                 counter += 1
             taxi_choice = int(input("Choose taxi: "))
-            if taxi_choice > len(taxi_list):
+            if taxi_choice >= len(taxi_list) or taxi_choice < 0:
                 print("Invalid taxi choice")
             else:
                 current_taxi = taxi_list[taxi_choice]
             print(f"Bill to date: ${total_bill:.2f}")
         elif user_input == "d":
-            print("d")
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+                print(f"Bill to date: ${total_bill:.2f}")
+            else:
+                distance_input = int(input("Drive how far? "))
+                current_taxi.start_fare()
+                current_taxi.drive(distance_input)
+                total_amount = current_taxi.get_fare()
+                total_bill += total_amount
+                print(f"Your {current_taxi.name} trip cost you ${total_amount:.2f}")
+                print(f"Bill to date: ${total_bill:.2f}")
         else:
-            "Invalid option"
-    print()
-
+            print("Invalid option")
+            print(f"Bill to date: ${total_bill:.2f}")
+        user_input = input("q)uit, c)hoose, d)rive\n>>> ").lower()
+    print(f"Total trip cost: ${total_bill:.2f}")
+    counter = 0
+    print("Taxi are now:")
+    for taxi in taxi_list:
+        print(f"{counter} - {taxi}")
+        counter += 1
 main()
